6a5ffa11e755079c3c8460abf236bd000d9839ce
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-29 13:37:48 -0300

    feat: ensure AxiosHttpClient returns correct response

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -1,8 +1,13 @@
 import { HttpPostClient } from '@/data/protocols/http'
-import axios from 'axios'
 
-export class AxiosHttpClient {
-  async post (params: HttpPostClient.Request): Promise<void> {
-    await axios.post(params.url, params.body)
+import axios, { AxiosResponse } from 'axios'
+
+export class AxiosHttpClient implements HttpPostClient {
+  async post (params: HttpPostClient.Request): Promise<HttpPostClient.Response<any>> {
+    const axiosResponse: AxiosResponse = await axios.post(params.url, params.body)
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    }
   }
 }
