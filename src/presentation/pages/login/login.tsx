c3c137b47eebe431605c736aa9362020b55a6466
Gerson Dantas <gerson.accari@hotmail.com>
2022-05-09 21:24:14 -0300

    feat: add login layout

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
 import Styles from './login.module.scss'
+
+import { Logo, Spinner } from 'src/presentation/components'
+
+import React from 'react'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.loginWrap}>
-      Hello
+      <form className={Styles.form}>
+        <header>
+          <Spinner />
+          <h1>
+            <Logo />
+          </h1>
+          <h2 className={Styles.title}>
+            Entre
+          </h2>
+          <h3 className={Styles.subtitle}>
+            E encante seus clietes
+          </h3>
+        </header>
+        <label htmlFor='email-input' className={Styles.emailLabel}>Email</label>
+        <input type='email' id='email-input' className={Styles.email} />
+        <label htmlFor='password-input' id='password-input' className={Styles.passwordLabel}>Senha</label>
+        <input type='password' id='password-input' className={Styles.password} />
+        <a href='#' className={Styles.forgot}>Esqueceu sua senha?</a>
+        <button className={Styles.button} >Fazer login</button>
+        <p>Não é cadastrado ainda?<a href='#'> Crie sua conta</a></p>
+      </form>
     </div>
   )
 }
