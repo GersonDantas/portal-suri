0c16f898b1a2b55a4c28fdb4ee71cf0cd07691af
Gerson Dantas <gerson.accari@hotmail.com>
2022-05-16 20:41:14 -0300

    test: ensure ValidationBuilder return EmailFieldValidation

--- src/__tests__/validation/validators/validation-builder.spec.ts
+++ src/__tests__/validation/validators/validation-builder.spec.ts
@@ -10,4 +10,11 @@ describe('ValidationBuilder', () => {
     const validations = sut.field(fieldName).required().build()
     expect(validations).toEqual([new RequiredFieldValidation(fieldName)])
   })
+
+  test('Should  return EmailFieldValidation', () => {
+    const fieldName = faker.database.column()
+
+    const validations = sut.field(fieldName).email().build()
+    expect(validations).toEqual([new RequiredFieldValidation(fieldName)])
+  })
 })
