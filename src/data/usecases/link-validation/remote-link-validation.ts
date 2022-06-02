8faba7c0b586155ca9cf83dd1ca8a86e9d6127db
Gerson Dantas <gerson.accari@hotmail.com>
2022-06-02 13:35:51 -0300

    feat: ensure RemoteLinkValidation returns error if link error

--- src/data/usecases/link-validation/remote-link-validation.ts
+++ src/data/usecases/link-validation/remote-link-validation.ts
@@ -1,4 +1,7 @@
+import { mockLinkValidationResponse } from 'src/__tests__/data/mock/mock-link-validation-response'
+import { LinkValidationResponseType } from 'src/data/models'
 import { HttpClient } from 'src/data/protocols/http'
+import { InvalidResetLinkError, LinkAlreadyUsedError } from 'src/domain/errors'
 import { LinkValidation } from 'src/domain/usecases'
 
 export class RemoteLinkValidation implements LinkValidation {
@@ -8,15 +11,20 @@ export class RemoteLinkValidation implements LinkValidation {
   ) { }
 
   async validate (params: LinkValidation.Params): Promise<RemoteLinkValidation.Model> {
-    this.httpClient.request({
+    const httpResponse = await this.httpClient.request({
       url: `${this.url}/${params.email}:${params.exp}`,
       body: JSON.stringify(params.hash),
       method: 'post'
     })
-    return Promise.resolve({
-      success: true,
-      type: 3
-    })
+
+    switch (httpResponse.body?.type) {
+      case LinkValidationResponseType.InvalidResetLink:
+        throw new InvalidResetLinkError()
+      case LinkValidationResponseType.LinkAlreadyUsed:
+        throw new LinkAlreadyUsedError()
+      default:
+        return Promise.resolve(mockLinkValidationResponse())
+    }
   }
 }
 
