f1d0b6b129888dfe7cccd2217509b762dafb0907
Gerson Dantas <gerson.accari@hotmail.com>
2022-06-02 13:36:53 -0300

    test: ensure RemoteLinkValidation returns error if link error

--- src/__tests__/data/usecases/remote-link-validation.spec.ts
+++ src/__tests__/data/usecases/remote-link-validation.spec.ts
@@ -1,8 +1,10 @@
 import { mockLinkValidationResponse } from '../mock/mock-link-validation-response'
 import { HttpClientSpy } from '../test'
 import { mockLinValidationParams } from 'src/__tests__/domain/mocks'
+import { LinkValidationResponseType } from 'src/data/models'
 import { HttpStatusCode } from 'src/data/protocols/http'
 import { RemoteLinkValidation } from 'src/data/usecases'
+import { InvalidResetLinkError, LinkAlreadyUsedError } from 'src/domain/errors'
 
 import faker from '@faker-js/faker'
 
@@ -42,5 +44,17 @@ describe('RemoteLinkValidation', () => {
     expect(httpClientSpy.body).toBe(JSON.stringify(params.hash))
     expect(httpClientSpy.url).toBe(`${url}/${params.email}:${params.exp}`)
     expect(httpClientSpy.method).toBe('post')
+  })
+
+  test('Should ensure RemoteLinkValidation returns InvalidResetLinkError or LinkAlreadyUsedError if user error', async () => {
+    const body = mockLinkValidationResponse()
+    const { sut } = makeSut({ body })
+    const promise = sut.validate(mockLinValidationParams())
+
+    await expect(promise).rejects.toThrow(
+      body.type === LinkValidationResponseType.InvalidResetLink
+        ? new InvalidResetLinkError()
+        : new LinkAlreadyUsedError()
+    )
   })
 })
