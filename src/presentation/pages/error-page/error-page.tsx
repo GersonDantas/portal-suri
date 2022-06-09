b18ff6c536f6f32731ac5e8893064f864e2fa976
Gerson Dantas <gerson.accari@hotmail.com>
2022-06-09 16:21:43 -0300

    feat: ensure ErrorPage render to correct error

--- src/presentation/pages/error-page/error-page.tsx
+++ src/presentation/pages/error-page/error-page.tsx
@@ -6,16 +6,22 @@ import { IonPage } from '@ionic/react'
 import React from 'react'
 import { useRecoilValue } from 'recoil'
 
-const ErrorPage: React.FC = () => {
+type Props = {
+  errorMessage?: string
+}
+
+const ErrorPage: React.FC<Props> = ({ errorMessage }) => {
   const mainError = useRecoilValue(errorPageState)
 
   return (
     <IonPage>
       <div className={Styles.errorPageWrap}>
         <div className={Styles.error}>
-          <span className={Styles.errorMessage}>{mainError}</span>
+          <span className={Styles.errorMessage} data-testid='main-error'>{
+            errorMessage || mainError || 'Algo errado aconteceu!'
+          }</span>
           <Button className={Styles.button}>
-            <p>Reload</p>
+            <p>Página de Login</p>
           </Button>
         </div>
       </div>
