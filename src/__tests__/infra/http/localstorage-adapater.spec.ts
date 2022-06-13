715b71d45b5a09ff815e13c45138f0c43c0244d1
Gerson Dantas <gerson.accari@hotmail.com>
2022-06-13 14:31:27 -0300

    test: ensure call localstorage.setItem with correct values

---
+++ src/__tests__/infra/http/localstorage-adapater.spec.ts
@@ -0,0 +1,24 @@
+import { SetStorage } from 'src/data/protocols/http'
+
+import 'jest-localstorage-mock'
+import faker from '@faker-js/faker'
+
+class LocalStorageAdapter implements SetStorage {
+  set (key: string, value: object): any {
+    localStorage.setItem(key, JSON.stringify(value))
+  }
+}
+
+describe('LocalStorageAdapter', () => {
+  beforeEach(() => localStorage.clear())
+
+  test('Should call localstorage.setItem with correct values', () => {
+    const sut = new LocalStorageAdapter()
+    const key = faker.database.column()
+    const value = { [faker.random.word()]: faker.random.word() }
+
+    sut.set(key, value)
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
+  })
+})
