cf3a6a4ca5dba8ec3b5e2b7a1c05c120aa0994ff
Gerson Dantas <gerson.accari@hotmail.com>
2022-06-15 17:21:29 -0300

    refactor: move SUT creation to a factory method

--- src/__tests__/validation/validators/compare-field-validation.spec.ts
+++ src/__tests__/validation/validators/compare-field-validation.spec.ts
@@ -3,13 +3,19 @@ import { CompareFieldValidation } from 'src/validation/validators'
 
 import faker from '@faker-js/faker'
 
+const makeSut = (field: string, fieldToCompare: string): CompareFieldValidation =>
+  new CompareFieldValidation(field, fieldToCompare)
+
 describe('CompareFieldValidation', () => {
   test('Should return error if compare is inválid', () => {
     const field = faker.database.column()
     const fieldToCompare = faker.database.column()
-    const sut = new CompareFieldValidation(field, fieldToCompare)
+    const sut = makeSut(field, fieldToCompare)
 
-    const error = sut.validate({})
+    const error = sut.validate({
+      [field]: 'any_value',
+      [fieldToCompare]: 'other_value'
+    })
 
     expect(error).toEqual(new InvalidFieldError())
   })
