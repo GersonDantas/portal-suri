b4a384d037fe0e517bc6382dbf5f11a5c2bf204f
Gerson Dantas <gerson.accari@hotmail.com>
2022-06-18 16:19:17 -0300

    feat: ensure RemoteResetPassword returns UnexpectedError  if other status code

--- src/data/usecases/reset-password/remote-reset-password.ts
+++ src/data/usecases/reset-password/remote-reset-password.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpStatusCode } from 'src/data/protocols/http'
-import { InvalidCredentialsError } from 'src/domain/errors'
+import { InvalidCredentialsError, UnexpectedError } from 'src/domain/errors'
 import { ResetPassword } from 'src/domain/usecases'
 
 export class RemoteResetPassword implements ResetPassword {
@@ -16,9 +16,12 @@ export class RemoteResetPassword implements ResetPassword {
     })
 
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return httpResponse.body
       case HttpStatusCode.badRequest:
         throw new InvalidCredentialsError()
+      default:
+        throw new UnexpectedError()
     }
-    return Promise.resolve(true)
   }
 }
