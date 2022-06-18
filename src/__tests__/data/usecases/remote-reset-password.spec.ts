f9b12b64f3bb381efe7e83428ab8c832333f57e2
Gerson Dantas <gerson.accari@hotmail.com>
2022-06-18 16:13:43 -0300

    test: ensure RemoteResetPassword throw InvalidCredentialError if return status code 400

--- src/__tests__/data/usecases/remote-reset-password.spec.ts
+++ src/__tests__/data/usecases/remote-reset-password.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientSpy } from '../test'
 import { mockResetPasswordParams } from 'src/__tests__/domain/mocks'
+import { HttpStatusCode } from 'src/data/protocols/http'
 import { RemoteResetPassword } from 'src/data/usecases'
+import { InvalidCredentialsError } from 'src/domain/errors'
 
 import faker from '@faker-js/faker'
 
@@ -30,5 +32,16 @@ describe('RemoteResetPassword', () => {
     expect(httpClientSpy.url).toBe(url)
     expect(httpClientSpy.body).toEqual(params)
     expect(httpClientSpy.method).toEqual('put')
+  })
+
+  it('Should throw InvalidCredentialsError if HttpClient return 400', async () => {
+    const { httpClientSpy, sut } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+
+    const promise = sut.reset(mockResetPasswordParams())
+
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError())
   })
 })
