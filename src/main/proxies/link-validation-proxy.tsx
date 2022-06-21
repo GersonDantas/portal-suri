dc6788abf4595a4e6ccb636e02294311def708c0
Gerson Dantas <gerson.accari@hotmail.com>
2022-06-21 16:20:36 -0300

    feat: ensure that ResetPassword will save the return in localstorage on success

--- src/main/proxies/link-validation-proxy.tsx
+++ src/main/proxies/link-validation-proxy.tsx
@@ -1,12 +1,13 @@
 import { linkValidationState } from './atoms'
+import { userInfoResetPasswordState } from 'src/__tests__/main/factories/mock/atoms'
 import { LinkValidation } from 'src/domain/usecases'
 import { ErrorPage } from 'src/presentation/pages'
 
 import { IonPage, useIonViewWillEnter } from '@ionic/react'
 import queryString from 'query-string'
 import React, { useCallback } from 'react'
-import { RouteProps, useLocation, Redirect } from 'react-router-dom'
-import { useRecoilState } from 'recoil'
+import { RouteProps, useLocation, Redirect, Route } from 'react-router-dom'
+import { useRecoilState, useRecoilValue } from 'recoil'
 
 type Props = RouteProps & {
   linkValidation: LinkValidation
@@ -15,6 +16,7 @@ type Props = RouteProps & {
 
 const LinkValidationProxy: React.FC<Props> = ({ linkValidation, fallbackRoute, ...props }): JSX.Element => {
   const [state, setState] = useRecoilState(linkValidationState)
+  const { setUserInfoResetPassword } = useRecoilValue(userInfoResetPasswordState)
   const { search } = useLocation()
   const { email, exp, k } = queryString.parse(search)
 
@@ -35,6 +37,7 @@ const LinkValidationProxy: React.FC<Props> = ({ linkValidation, fallbackRoute, .
       const { success } = await linkValidation.validate({
         email: `${email}`, exp: `${exp}`, hash: `${k}`
       })
+      await setUserInfoResetPassword({ email: `${email}`, hash: `${k}` })
       setState(old => ({
         ...old,
         success
@@ -50,7 +53,7 @@ const LinkValidationProxy: React.FC<Props> = ({ linkValidation, fallbackRoute, .
   const RenderComponent = useCallback((): JSX.Element => {
     if (state.urlWithParams) {
       return state.success
-        ? <Redirect to={`/mudar-senha/${email}/${k}`} />
+        ? <Route {...props} />
         : <ErrorPage goFallBack={goFallBackRoute} errorMessage={state.mainError} />
     } else {
       return <Redirect to={fallbackRoute} />
